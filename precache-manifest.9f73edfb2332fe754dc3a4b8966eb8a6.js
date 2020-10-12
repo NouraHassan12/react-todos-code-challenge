cef220d6811367c1125865655594591a2f82168b
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-10-12 05:46:44 +0200

    Updates

---
+++ precache-manifest.9f73edfb2332fe754dc3a4b8966eb8a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fa42e50e79efebc85a357060da3461",
+    "url": "/react-todos-code-challenge/index.html"
+  },
+  {
+    "revision": "17e014dbc972bde70f9c",
+    "url": "/react-todos-code-challenge/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b4edae4e54e2cd436dab",
+    "url": "/react-todos-code-challenge/static/css/main.10bec95a.chunk.css"
+  },
+  {
+    "revision": "17e014dbc972bde70f9c",
+    "url": "/react-todos-code-challenge/static/js/2.1c2c9aa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todos-code-challenge/static/js/2.1c2c9aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4edae4e54e2cd436dab",
+    "url": "/react-todos-code-challenge/static/js/main.2235612d.chunk.js"
+  },
+  {
+    "revision": "7b064140e3e3f0defd8e",
+    "url": "/react-todos-code-challenge/static/js/runtime-main.f6402a35.js"
+  }
+]);
